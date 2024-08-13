31911aa5eaf8e589b2026676bee919ce7b11e1a1
Strangexz <ja_figueroa@outlook.es>
2024-08-13 14:47:34 -0600

    feat: controlador dle producto

---
+++ src/main/java/hn/com/js/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package hn.com.js.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hn.com.jf.models.Product;
+import hn.com.jf.services.ProductServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+	private ProductServiceImpl service = new ProductServiceImpl();
+
+	@GetMapping
+	public List<Product> list() {
+		return service.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Product show(@PathVariable Long id) {
+		return service.findById(id);
+	}
+
+}
