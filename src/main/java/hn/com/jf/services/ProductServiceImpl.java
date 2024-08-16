4f06299a9b56cc19f2f2f8e6fc04ddc6fa059c7e
Strangexz <ja_figueroa@outlook.es>
2024-08-16 16:43:29 -0600

    feat: implementación de los valores del archivo de propiedades personalizado

--- src/main/java/hn/com/jf/services/ProductServiceImpl.java
+++ src/main/java/hn/com/jf/services/ProductServiceImpl.java
@@ -3,25 +3,42 @@ package hn.com.jf.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import hn.com.jf.models.Product;
 import hn.com.jf.repositories.ProductRepository;
 
-@Component
+@Service
 public class ProductServiceImpl implements ProductService {
+	@Value("${config.price.tax}")
+	private Double tax;
 
-	@Autowired
+	/* Inyectando por atributo */
+//	@Autowired
+//	@Qualifier("productFoo")
 	private ProductRepository repository;
+
+	/* Inyección por constructor */
+//	public ProductServiceImpl(ProductRepository repository) {
+	public ProductServiceImpl(@Qualifier("productRepositoryJson") ProductRepository repository) {
+		this.repository = repository;
+	}
 
 	@Override
 	public List<Product> findAll() {
 		return repository.findAll().stream().map(p -> {
-			Double priceTax = p.getPrice() * 1.25d;
+			Double priceTax = p.getPrice() * tax;
 //			p.setPrice(priceTax.longValue());
+
 //			Product newProd = new Product(p.getId(), p.getName(), priceTax.longValue());
 			Product newProd = (Product) p.clone();
+
+//			p.setPrice(priceTax.longValue());
+//			return p;
+
 			newProd.setPrice(priceTax.longValue());
 			return newProd;
 		}).collect(Collectors.toList());
